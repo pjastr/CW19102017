62792db80a995a04d75afef1d86ee89809b240c8
pjastr <24464854+pjastr@users.noreply.github.com>
2017-10-19 18:38:58 +0200

    Add files via upload

---
+++ ConsoleApplication33/ConsoleApplication33/Samochod.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication33
+{
+    class Samochod
+    {
+        private string model;
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        private double cena;
+
+        public double Cena
+        {
+            get { return cena; }
+            set { cena = value; }
+        }
+
+        public string Marka { get; set; }
+
+        public double pojemnosc;
+        
+    }
+}
